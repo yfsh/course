58bcd1a927c7fbb31f41a87b0c9ed81f7e755c18
yfsh <136050581@qq.com>
2020-05-01 17:02:41 +0800

    1，解决注册中心服务启动失败的问题 2，优化启动日志显示

--- eureka/src/main/java/com/yfsh/EurekaApplication.java
+++ eureka/src/main/java/com/yfsh/EurekaApplication.java
@@ -1,13 +1,29 @@
 package com.yfsh;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
 
+	//log日志框架的固定写法
+	private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(EurekaApplication.class, args);
+//	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication app = new SpringApplication(EurekaApplication.class);
+		//env.getProperty("server.port")获取应用的端口
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("Eureka地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 
 }
