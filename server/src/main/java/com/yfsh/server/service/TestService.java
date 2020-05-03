af0c6510ff91c5dadcaa966663e4473114539999
yfsh <136050581@qq.com>
2020-05-03 21:59:23 +0800

    1，Example使用示例 使用Example可以快速完成单表的增删改查，熟练掌握后对代码开发效率将有一个质的飞跃

--- server/src/main/java/com/yfsh/server/service/TestService.java
+++ server/src/main/java/com/yfsh/server/service/TestService.java
@@ -1,6 +1,7 @@
 package com.yfsh.server.service;
 
 import com.yfsh.server.domain.Test;
+import com.yfsh.server.domain.TestExample;
 import com.yfsh.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,10 @@ public class TestService {
     private TestMapper testMapper;
 
     public List<Test> list(){
-        return testMapper.list();
+        TestExample testExample = new TestExample();
+        testExample.createCriteria().andIdEqualTo("4");
+        testExample.setOrderByClause("id desc");
+        return testMapper.selectByExample(testExample);
     }
 
 }
